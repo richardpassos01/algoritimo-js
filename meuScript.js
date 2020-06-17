8bc71d0292c5203767e0beff7f1b64c8f1cd0649
richardpassos01 <richard.passos@hotmail.com>
2020-06-17 20:55:41 -0300

    feat(index): first commit

---
+++ meuScript.js
@@ -0,0 +1,21 @@
+let saldoDisponivel = 1000;
+
+alert('insira seu cartão');
+
+alert('Seu saldo disponivel é' + saldoDisponivel + 'R$');
+
+let valorSaque = window.prompt('Quanto deseja sacar');
+
+if(valorSaque <= saldoDisponivel) {
+    saldoDisponivel = saldoDisponivel - valorSaque;
+
+    console.log('vc sacou seu dinheiro');
+
+    console.log('Seu novo saldo é de', saldoDisponivel);
+    
+    console.log('Obrigado pela preferencia');
+} else {
+    alert('Tu nao tem dinheiro rapá.');
+}
+
+
